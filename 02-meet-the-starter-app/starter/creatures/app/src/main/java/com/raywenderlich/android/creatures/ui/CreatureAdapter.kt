77f83a9425974a7cfc65c37a84355fd54315ec71
Herve Desrosiers <hdesrosiers@stingray.com>
2021-06-03 07:55:35 -0400

    click listener on view holder

--- 02-meet-the-starter-app/starter/creatures/app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
+++ 02-meet-the-starter-app/starter/creatures/app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
@@ -1,27 +1,48 @@
 package com.raywenderlich.android.creatures.ui
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.raywenderlich.android.creatures.R
-import com.raywenderlich.android.creatures.app.inflate
 import com.raywenderlich.android.creatures.model.Creature
+import kotlinx.android.synthetic.main.list_item_creature.view.*
 
 class CreatureAdapter(private val creatures: List<Creature>): RecyclerView.Adapter<CreatureAdapter.ViewHolder>() {
 
-    inner class ViewHolder(v: View): RecyclerView.ViewHolder(v) {
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private lateinit var creature: Creature
 
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(creature: Creature) {
+            this.creature = creature
+            val context = itemView.context
+            itemView.creatureImage.setImageResource(context.resources.getIdentifier(creature.uri, null, context.packageName))
+            itemView.fullName.text = creature.fullName
+            itemView.nickname.text = creature.nickname
+        }
+
+        override fun onClick(v: View?) {
+            v?.let {
+                val context = it.context
+                val intent = CreatureActivity.newIntent(context, creature.id)
+                context.startActivity(intent)
+            }
+        }
     }
 
     //only called a few times (enough to fill the container plus a few extras above and below)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        val itemView: View = LayoutInflater.from(parent.context).inflate(R.layout.list_item_creature, parent, false)
-//        return  ViewHolder(itemView)
-        return ViewHolder(parent.inflate(R.layout.list_item_creature)) //uses inflate of ViewGroupExtensions
+        val itemView: View = LayoutInflater.from(parent.context).inflate(R.layout.list_item_creature, parent, false)
+        return  ViewHolder(itemView)
+//        return ViewHolder(parent.inflate(R.layout.list_item_creature)) //uses inflate of ViewGroupExtensions
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        val creature = CreatureStore.getCreatures()[position]
+        holder.bind(creatures[position])
 
     }
 
